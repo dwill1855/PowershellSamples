806aac36113b51039ed837abfab59653fb16b06c
Derek Williams <derek.williams@crosswinds.io>
2018-04-24 20:40:44 -0400

    Updated variable after preparing for git

--- Azure/Enable-Http2OnWebApps.ps1
+++ Azure/Enable-Http2OnWebApps.ps1
@@ -7,7 +7,7 @@ Login-AzureRmAccount
 Select-AzureRmSubscription -Subscription $Subscription
 $apps = Get-AzureRmWebApp
 foreach($a in $apps) {
-    $c= Get-AzureRmResource -ResourceType "Microsoft.Web/sites/config" -ResourceName $b.Name -ResourceGroupName $b.ResourceGroup -ApiVersion 2016-08-01
+    $c= Get-AzureRmResource -ResourceType "Microsoft.Web/sites/config" -ResourceName $a.Name -ResourceGroupName $a.ResourceGroup -ApiVersion 2016-08-01
     $c.Properties.http20Enabled = $true
     $c | Set-AzureRmResource -ApiVersion 2016-08-01 -force 
 }
