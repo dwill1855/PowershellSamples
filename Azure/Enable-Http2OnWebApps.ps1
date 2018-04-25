cbe8a8c6a5ca6f9a39d1d8f34b3ac26101456979
Derek Williams <derek.williams@crosswinds.io>
2018-04-24 20:24:20 -0400

    Added Enable-Http2OnAllWebApps

---
+++ Azure/Enable-Http2OnWebApps.ps1
@@ -0,0 +1,13 @@
+﻿<#
+    Enable-Http2OnWebApps.ps1
+#>
+$Subscription = "<MY SUBSCRIPTION>"
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -Subscription $Subscription
+$apps = Get-AzureRmWebApp
+foreach($a in $apps) {
+    $c= Get-AzureRmResource -ResourceType "Microsoft.Web/sites/config" -ResourceName $b.Name -ResourceGroupName $b.ResourceGroup -ApiVersion 2016-08-01
+    $c.Properties.http20Enabled = $true
+    $c | Set-AzureRmResource -ApiVersion 2016-08-01 -force 
+}
